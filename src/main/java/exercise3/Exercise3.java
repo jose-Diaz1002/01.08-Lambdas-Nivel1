f1a267ebf7b4062bcfd74644b8957d14b1ec77cb
JoseDiaz <jldi1002@hotmail.com>
2025-04-07 15:16:12 +0200

    borrando comentarios

--- src/main/java/exercise3/Exercise3.java
+++ src/main/java/exercise3/Exercise3.java
@@ -1,8 +1,4 @@
 package exercise3;
-/*
-Crea una lista con los nombres de los meses del año. Imprime todos los elementos de la
-lista con una lambda.
- */
 
 import java.util.List;
 
@@ -10,6 +6,6 @@ public class Exercise3 {
 
     public static void montsList(List<String> months) {
 
-       months.stream().forEach(System.out::println);
+        months.stream().forEach(System.out::println);
     }
 }
